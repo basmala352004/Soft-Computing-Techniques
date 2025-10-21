c74a26668da372d8bb6e3f90fb82403d4b857485
Abdelhaleem <abdelhaleem1000@gmail.com.com>
2025-10-22 01:28:42 +0300

    Added 1 binary, 2 integer, 1 fp mutation strategies

---
+++ src/main/java/com/scproject/mutation/SwapMutation.java
@@ -0,0 +1,49 @@
+package com.scproject.mutation;
+
+import com.scproject.chromosome.Chromosome;
+import com.scproject.chromosome.IntegerChromosome;
+
+import java.util.Random;
+
+
+public class SwapMutation implements MutationStrategy {
+
+    private final Random random = new Random();
+    private double mutationRate = 0.1;
+
+    @Override
+    public void setMutationRate(double mutationRate) {
+        this.mutationRate = mutationRate;
+    }
+
+    @Override
+    public void mutate(Chromosome chromosome) {
+        if (!(chromosome instanceof IntegerChromosome)) {
+            throw new IllegalArgumentException("SwapMutation only applies to IntegerChromosome");
+        }
+
+        IntegerChromosome intChrom = (IntegerChromosome) chromosome;
+        Object[] genes = intChrom.getGenes();
+
+        if (random.nextDouble() < mutationRate) {
+            int index1 = random.nextInt(genes.length);
+            int index2 = random.nextInt(genes.length);
+
+            while (index1 == index2) {
+                index2 = random.nextInt(genes.length);
+            }
+
+            Object temp = genes[index1];
+            genes[index1] = genes[index2];
+            genes[index2] = temp;
+
+            intChrom.setGenes(genes);
+            intChrom.resetEvaluation();
+        }
+    }
+
+    @Override
+    public boolean isApplicable(Chromosome chromosome) {
+        return chromosome instanceof IntegerChromosome;
+    }
+}
