1b8bcd5bdbe27808466f1475b8134673fbf5dd70
AmiraEmad-04 <ameraemadfouad@gmail.com>
2025-10-23 11:28:14 +0300

    constraints and fitness

--- src/main/java/com/scproject/Main.java
+++ src/main/java/com/scproject/Main.java
@@ -1,5 +1,8 @@
 package com.scproject;
 
+import java.util.Map;
+import java.util.Set;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -13,5 +16,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+
     }
 }
